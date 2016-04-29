4698e62b56b6ce21465efe68c2af7a6acd559a8d
Iuri André Stona <iuristona@hotmail.com>
2016-04-29 16:31:06 -0300

    spinner loader

--- app/customers/customers.service.ts
+++ app/customers/customers.service.ts
@@ -11,33 +11,31 @@ export class CustomersService {
     }
 
     getAll(params?: any) {
-        
         let search: URLSearchParams = new URLSearchParams();
-        
+
         for (let key in params) {
             search.set(key, params[key])
         }
-        
+
         let headers = new Headers();
         //headers.append('Content-Type', 'application/x-www-form-urlencoded');
         headers.set('Accept', 'application/json, text/plain, */*');
-                
+
         return this._http.get('http://services.odata.org/Northwind/Northwind.svc/Customers', {
             headers: headers,
             search: search
-        })
-        .map(n => n.json())
-        .catch(this.handleError);                
+        }).map(n => n.json()
+        ).catch(this.handleError);
     }
-    
+
     public save(model: any) {
-      
+
         // return new Promise<boolean>((resolve, reject) => {
         //     setTimeout(function() {
         //       resolve(true);
         //     }, 500);
         // });
-      
+
         return Promise.resolve(true);
     }
 
@@ -47,7 +45,7 @@ export class CustomersService {
         console.error(errMsg); // log to console instead
         return Observable.throw(errMsg);
     }
-    
+
     // private _contacts = [];
     // private get contacts(): Contact[] {
     //     let contacts: Contact[] = <Contact[]>JSON.parse(localStorage.getItem('contacts')) || [];
@@ -62,7 +60,7 @@ export class CustomersService {
     //         //contacts = contacts.sort((a, b) => a.name > b.name ? 1 : a.name < b.name ? -1 : 0);
 
     //         contacts = Utils.orderBy(contacts, n => n.name);
-            
+
     //         localStorage.setItem('contacts', JSON.stringify(contacts));
     //     }
 
