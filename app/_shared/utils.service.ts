645e6121e279d54009fad9cf34860b5fd87c0ff9
Iuri André Stona <iuristona@hotmail.com>
2016-05-12 14:21:26 -0300

    validation

--- app/_shared/utils.service.ts
+++ app/_shared/utils.service.ts
@@ -3,9 +3,9 @@ import {Router} from 'angular2/router';
 
 @Injectable()
 export class UtilsService {
-    
+
     constructor(private router: Router) { }
-    
+
     isRouteActive(linkParams: any[]) {
         return this.router.isRouteActive(this.router.generate(linkParams));
     }
